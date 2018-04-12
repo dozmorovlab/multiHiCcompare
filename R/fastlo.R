71e608cb175f8055f4be65d5e05222c85200ad97
jstansfield0 <stansfieldjc@vcu.edu>
2018-04-12 20:32:16 +0000

    work on fastlo

--- R/fastlo.R
+++ R/fastlo.R
@@ -12,6 +12,22 @@ fastlo <- function(hicexp, iterations = 3, span = NA, parallel = FALSE, verbose 
 # perform fastlo on a condition
 .fastlo_condition <- function(hic_table, iterations, span, parallel, verbose, Plot) {
   # split up data by chr
-  table_by_chr <- split(hic_table, hic_table$chr)
-  
+  table_list <- split(hic_table, hic_table$chr)
+  # for each chr create list of distance matrices
+  table_list <- lapply(table_list, .get_dist_tables)
+  # combine list of lists into single list of tables
+  table_list <- do.call(c, table_list)
+}
+
+.get_dist_tables <- function(chr_table) {
+  all_dist <- sort(unique(chr_table$D))
+  dist_85 <- ceiling(0.85 * length(all_dist))
+  table_by_dist <- list()
+  idx <- 1
+  for(i in all_dist[1:dist_85]) {
+    table_by_dist[[idx]] <- chr_table[D == i,]
+    idx <- idx + 1
+  }
+  table_by_dist[[idx]] <- chr_table[D > all_dist[dist_85],]
+  return(table_by_dist)
 }
