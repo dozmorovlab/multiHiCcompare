5860cd3e8d1617f2f3fe9a102ea04d81f986ece3
jstansfield0 <stansfieldjc@vcu.edu>
2018-04-13 15:54:08 +0000

    finish fastlo

--- R/fastlo.R
+++ R/fastlo.R
@@ -1,10 +1,28 @@
 #' Perform fast loess normalization on a Hi-C experiment
 #' 
+#' @param hicexp A hicexp object
+#' @param iterations The number of iterations (cycles) 
+#'     for fastlo to proceed through.
+#' @param span The span of loess fitting. Defaults to 0.7. To
+#'     automatically calculate a span using the GCV set 
+#'     span = NA. However note that setting span = NA will
+#'     significantly slow down the normalization process.
+#' @param parallel Logical. Should parallel processing be
+#'     used?
 #' 
+#' @details This function performs the fast loess (fastlo)
+#'    normalization procedure on an hicexp object. 
+#' 
+#' @export
+#' @importFrom BiocParallel bplapply
+#' @importFrom dplyr %>%
 
-fastlo <- function(hicexp, iterations = 3, span = NA, parallel = FALSE, verbose = TRUE, Plot = TRUE) {
+fastlo <- function(hicexp, iterations = 3, span = 0.7, parallel = FALSE, verbose = TRUE, Plot = TRUE) {
   # input conditions to fastlo
   normalized <- lapply(hicexp@hic_tables, .fastlo_condition, iterations = iterations, span = span, parallel = parallel, verbose = verbose, Plot = Plot)
+  # put back into hicexp object
+  hicexp@hic_tables <- normalized
+  return(hicexp)
 }
 
 # bacground functions
@@ -17,8 +35,18 @@ fastlo <- function(hicexp, iterations = 3, span = NA, parallel = FALSE, verbose 
   table_list <- lapply(table_list, .get_dist_tables)
   # combine list of lists into single list of tables
   table_list <- do.call(c, table_list)
+  # apply fastlo to list
+  if (parallel) {
+    normalized <- BiocParallel::bplapply(table_list, .fastlo, span = span)
+  } else {
+    normalized <- lapply(table_list, .fastlo, span = span)
+  }
+  # recombine list of tables
+  hic_table <- rbindlist(normalized)
+  return(hic_table)
 }
 
+# make list of tables by distance
 .get_dist_tables <- function(chr_table) {
   all_dist <- sort(unique(chr_table$D))
   dist_85 <- ceiling(0.85 * length(all_dist))
@@ -30,4 +58,38 @@ fastlo <- function(hicexp, iterations = 3, span = NA, parallel = FALSE, verbose 
   }
   table_by_dist[[idx]] <- chr_table[D > all_dist[dist_85],]
   return(table_by_dist)
+}
+
+# perform fastlo on table
+.fastlo <- function(tab, span) {
+  # make matrix of IFs
+  IF_mat <- tab[, 5:(ncol(tab)), with = FALSE] %>% as.matrix()
+  n <- ncol(IF_mat)
+  # outer loop for number of cycles
+  for (i in 1:iterations) {
+    # calculate rowmeans (A) 
+    A <- rowMeans(IF_mat, na.rm = TRUE)
+    for (j in 1:n) {
+      M <- IF_mat[,j] - A
+      # fit loess curve
+      if (is.na(span)) {
+        f <- .loess.as(x = A, y = M, degree = degree, 
+                       criterion = loess.criterion,
+                       control = loess.control(surface = "interpolate",
+                                               statistics = "approximate", trace.hat = "approximate"))
+      } else {
+        f <- .loess.as(x = A, y = M, degree = degree, user.span = span,
+                       criterion = loess.criterion,
+                       control = loess.control(surface = "interpolate",
+                                               statistics = "approximate", trace.hat = "approximate"))
+      }
+      # adjust 
+      IF_mat[,j] <- IF_mat[,j] - f$fitted
+    }
+  }
+  # deal with negative values after normalization
+  IF_mat[IF_mat < 0] <- 0
+  # rebuild table
+  tab <- cbind(tab[, 1:4, with = FALSE], IF_mat)
+  return(tab)
 }
---
+++ R/plot_functions.R
@@ -0,0 +1,5 @@
+#' Make MD plots for all combinations of a condition
+#' 
+
+
+# Plan: make a grid of MD plots for all the pairwise combinations of comparisons between replicates in a condition
