efaf6748c6ba5e203882cc4be2c839b7a9834e7f
jstansfield0 <stansfieldjc@vcu.edu>
2018-04-20 20:50:25 +0000

    more work on glm

--- R/glm.R
+++ R/glm.R
@@ -4,7 +4,7 @@
 #' @import edgeR
 #' @importFrom dplyr %>%
 
-hic_exactTest <- function(hicexp, parallel = FALSE, p.method = "fdr") {
+hic_exactTest <- function(hicexp, parallel = FALSE, p.method = "fdr", Plot = TRUE) {
   # check to make sure hicexp is normalized
   if (!hicexp@normalized) {
     warning("You should normalize the data before entering it into hic_glm")
@@ -47,7 +47,9 @@ hic_exactTest <- function(hicexp, parallel = FALSE, p.method = "fdr") {
   hicexp@comparison <- comparison
   
   # plot
-  MD.composite(hicexp)
+  if (Plot) {
+    MD.composite(hicexp)
+  }
   
   # return results
   return(hicexp)
@@ -68,11 +70,27 @@ hic_exactTest <- function(hicexp, parallel = FALSE, p.method = "fdr") {
 
 
 #' Function to perform GLM differential analysis on Hi-C experiment
+#' 
+#' @param hicexp A hicexp object,
+#' @param design A design matrix for the GLM.
+#' @param contrast A vector containing the contrast you want to test 
+#' @param coef A value or vect or the coefficients to test.
+#' @param method The test method to be performed. Should be one of
+#'    "QLFTest", "LRTest", or "Treat".
+#' @param M The log2 fold change value for a TREAT analysis.
+#' @param parallel Logical, Shoudl parallel processing be used?
 
-hic_glm <- function(hicexp, design, parallel = FALSE) {
+hic_glm <- function(hicexp, design, contrast = NA, coef = NA, method = "QLFTest", M = 1, parallel = FALSE) {
   # check to make sure hicexp is normalized
   if (!hicexp@normalized) {
     warning("You should normalize the data before entering it into hic_glm")
+  }
+  # contrast & coef input
+  if (is.na(contrast) & is.na(coef)) {
+    stop("You must enter a value for contrast or a coef, but not both")
+  }
+  if (!is.na(contrast) & !is.na(coef)) {
+    stop("Please enter either a value for contrast or coef but NOT both.")
   }
   # First need to split up hic_table by chr and then by distance
   # split up data by chr
@@ -90,6 +108,54 @@ hic_glm <- function(hicexp, design, parallel = FALSE) {
   } else {
     dge_list <- lapply(dge_list, edgeR::estimateDisp, design = design)
   }
+  
+  # fit the GLM
+  if (parallel) {
+    fit <- BiocParallel::bplapply(dge_list, edgeR::glmQLFit, design = design)
+  } else {
+    fit <- lapply(dge_list, edgeR::glmQLFit, design = design)
+  }
+  
+  ## Perform test based on method and contrast/coef specified 
+  # ?? should probably add parallel option here too
+  # apply QL F-test test
+  if (method == "QLFTest") {
+    if (is.na(coef)) {
+      result <- lapply(fit, edgeR::glmQLFTest, contrast = contrast)
+    } else {
+      result <- lapply(fit, edgeR::glmQLFTest, coef = coef)
+    }
+  }
+  # Likelihood Ratio Test
+  if (method == "LRTest") {
+    if (is.na(coef)) {
+      result <- lapply(fit, edgeR::glmLRT, contrast = contrast)
+    } else {
+      result <- lapply(fit, edgeR::glmLRT, coef = coef)
+    }
+  }
+  # TREAT Analysis based on a minimum log2 fold change specified as M
+  if (method == "Treat") {
+    if (is.na(coef)) {
+      result <- lapply(fit, edgeR::glmTreat, contrast = contrast, lfc = M)
+    } else {
+      result <- lapply(fit, edgeR::glmTreat, coef = coef, lfc = M)
+    }
+  }
+  
+  # format results for differentially interacting regions
+  
+}
+
+
+# reformat results of GLM
+.glm_reformat <- function(result, hic_table, p.method) {
+  # create table of location info and p-value results
+  result <- cbind(hic_table[, 1:4, with = FALSE], result$table)
+  colnames(result)[ncol(result)] <-"p.value"
+  # adjust p-values
+  result$p.adj <- p.adjust(result$p.value, method = p.method)
+  return(result)
 }
 
 
