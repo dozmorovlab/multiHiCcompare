86b4b42ae95b380a16970ba1a4e882fe7e7be9cb
jstansfield0 <stansfieldjc@vcu.edu>
2018-07-13 17:10:59 +0000

    further speed up for r cmd check

--- tests/testthat/test_comparison.R
+++ tests/testthat/test_comparison.R
@@ -1,10 +1,8 @@
 test_that('exact test works', {
   library(testthat)
   library(HiCcompare2)
-  data("r1", "r2", "r3", "r4", "r5", "r6", "hicexp")
-  groups <- c(1, 1, 2, 2)
-  hicexp_new <- make_hicexp(r1, r2, r3, r4,groups = groups)
-  hicexp_new <- fastlo(hicexp_new, verbose = FALSE, Plot = FALSE)
+  data("hicexp2")
+  hicexp_new <- fastlo(hicexp2, verbose = FALSE, Plot = FALSE)
   hicexp_new <- hic_exactTest(hicexp_new, Plot = FALSE)
   expect_gt(nrow(hicexp_new@comparison), 1)
   
@@ -19,10 +17,8 @@ test_that('exact test works', {
 test_that('glm works', {
   library(testthat)
   library(HiCcompare2)
-  data("r1", "r2", "r3", "r4", "r5", "r6", "hicexp")
-  groups <- c(1, 1, 2, 2)
-  hicexp_new <- make_hicexp(r1, r2, r3, r4,groups = groups)
-  hicexp_new <- fastlo(hicexp_new, verbose = FALSE, Plot = FALSE)
+  data("hicexp2")
+  hicexp_new <- fastlo(hicexp2, verbose = FALSE, Plot = FALSE)
   d <- model.matrix(~factor(hicexp_new@metadata$group))
   hicexp_new <- hic_glm(hicexp_new, design = d, coef = 2, method = "QLFTest", Plot = FALSE)
   expect_gt(nrow(hicexp_new@comparison), 1)
